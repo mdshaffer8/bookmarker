53f89615f8dd6dbf2f970bd84711301f55fa52eb
mdshaffer8 <mdshaffer8@live.com>
2018-06-17 15:35:20 -0400

    changed button color

--- js/main.js
+++ js/main.js
@@ -86,7 +86,7 @@ function fetchBookmarks(){
 
     	bookmarksResults.innerHTML += '<div class="card card-body bg-light">'+
                                   '<h3>'+name+
-                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a class="btn btn-success" target="_blank" href="'+url+'">Visit</a> ' +
                                   ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
                                   '</h3>'+
                                   '</div>' +
